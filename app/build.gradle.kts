ec4220e6d14634915156c4fb059fd98f54840f64
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-02-21 23:09:37 +0530

    manage versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.hathibelagal.eidetic"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.3.1"
+        versionCode = 7
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
