c3ac4541a573757d2e574ae93568d4bc04101338
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-04-27 03:28:12 +0530

    grayscale instead of gameover

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.hathibelagal.eidetic"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.4.3"
+        versionCode = 12
+        versionName = "1.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
